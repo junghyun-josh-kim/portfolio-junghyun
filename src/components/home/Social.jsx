6d8c5a8c8d7f86cb5ba3ce32135f17a9ddfc3778
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-11-01 01:51:56 -0400

    Home: Profile img

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+const Social = () => {
+  return (
+    <div className='home__social'>
+      <a
+        href='https://github.com/junghyun-josh-kim'
+        className='home__social-icon'
+        target='_blank'
+      >
+        <i class='uil uil-github-alt'></i>
+      </a>
+      <a
+        href='https://www.facebook.com/junghyun.kim.739978'
+        className='home__social-icon'
+        target='_blank'
+      >
+        <i class='uil uil-facebook-f'></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
