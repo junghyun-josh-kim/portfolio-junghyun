c43f50d6428142faae884294bcfbd3edf1b415dd
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-11-02 15:52:26 -0400

    Testimonials: Swiper JS

--- src/components/tesimonials/Testimonials.jsx
+++ src/components/tesimonials/Testimonials.jsx
@@ -1,5 +1,9 @@
 import './testimonials.css';
 import { Data } from './Data';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper';
 
 const Testimonials = () => {
   return (
@@ -7,17 +11,35 @@ const Testimonials = () => {
       <h2 className='section__title'>Testimonials</h2>
       <span className='section__subtitle'>Former Colleagues</span>
 
-      <div className='testimonials__container'>
+      <Swiper
+        className='testimonials__container'
+        loop={true}
+        grabCursor={true}
+        spaceBetween={24}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          576: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+          },
+        }}
+        modules={[Pagination]}
+      >
         {Data.map(({ id, image, title, description }) => {
           return (
-            <div className='testimonial__card' key={id}>
+            <SwiperSlide className='testimonial__card' key={id}>
               <img src={image} alt='' className='testimonial__img' />
               <h3 className='testimonial__name'>{title}</h3>
               <p className='testimonial__description'>{description}</p>
-            </div>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
