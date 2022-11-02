2595459864a71c3b88202ed4870e0cb3cdecd772
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-11-02 15:40:12 -0400

    Testimonials: JSX Data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
+import Testimonials from './components/tesimonials/Testimonials';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Skills />
         <Services />
         <Qualification />
+        <Testimonials />
       </main>
     </>
   );
---
+++ src/components/tesimonials/Data.jsx
@@ -0,0 +1,26 @@
+import Image1 from '../../assets/testimonial1.png';
+import Image2 from '../../assets/testimonial2.png';
+
+export const Data = [
+  {
+    id: 1,
+    image: Image1,
+    title: 'Hayley Kiyoko',
+    description:
+      'I really good job, he is the best problem solver. I am rooting for his new journey!',
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: 'Josh Kim',
+    description:
+      'I really good job, he is the best problem solver. I am rooting for his new journey!',
+  },
+  {
+    id: 3,
+    image: Image1,
+    title: 'Andrew Kim',
+    description:
+      'I really good job, he is the best problem solver. I am rooting for his new journey!',
+  },
+];
