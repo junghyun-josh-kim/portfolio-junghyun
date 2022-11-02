16f0fa597b976efb2dc682878412f07a310c715a
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-11-01 23:30:18 -0400

    Skills: JSX, CSS

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,58 @@
+const Backend = () => {
+  return (
+    <div className='skills__content'>
+      <h3 className='skills__title'>Back End</h3>
+
+      <div className='skills__box'>
+        <div className='skills__group'>
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>Python</h3>
+              <span className='skills__level'>Basic</span>
+            </div>
+          </div>
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>Django</h3>
+              <span className='skills__level'>Basic</span>
+            </div>
+          </div>
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>Firebase</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+        </div>
+        <div className='skills__group'>
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>Node.JS</h3>
+              <span className='skills__level'>Basic</span>
+            </div>
+          </div>
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>MySQL</h3>
+              <span className='skills__level'>Basic</span>
+            </div>
+          </div>
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>Postman</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
