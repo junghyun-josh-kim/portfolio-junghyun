2ed7f83a0e615f35f7a0c2bacb5f1ac6e09aca7b
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-11-01 22:31:16 -0400

    About: JSX, CSS

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,23 @@
+const Info = () => {
+  return (
+    <div className='about__info grid'>
+      <div className='about__box'>
+        <i class='bx bx-award about__icon'></i>
+        <h3 className='about__title'>Experience</h3>
+        <span className='about__subtitle'>1+ Year Working</span>
+      </div>
+      <div className='about__box'>
+        <i class='uil uil-syringe about__icon'></i>
+        <h3 className='about__title'>Problem Solver</h3>
+        <span className='about__subtitle'>4+ Years General Manager</span>
+      </div>
+      <div className='about__box'>
+        <i class='uil uil-game-structure about__icon'></i>
+        <h3 className='about__title'>Growth mindset</h3>
+        <span className='about__subtitle'>Highly motivated</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
