daa6caa4c98d4bf9e868702218130eac9d73341d
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-11-02 23:46:44 -0400

    Portfolio: CSS responsive

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -1,21 +1,48 @@
 import './work.css';
 import { projectsData, projectsNav } from './Data';
 import WorkItems from './WorkItems';
+import { useEffect, useState } from 'react';
 
 const Works = () => {
+  const [item, setItem] = useState({ name: 'All' });
+  const [projects, setProjects] = useState([]);
+  const [active, setActive] = useState(0);
+
+  useEffect(() => {
+    if (item.name === 'All') {
+      setProjects(projectsData);
+    } else {
+      const newProjects = projectsData.filter((project) => {
+        return project.category === item.name;
+      });
+      setProjects(newProjects);
+    }
+  }, [item]);
+
+  const handleClick = (event, index) => {
+    setItem({ name: event.target.textContent });
+    setActive(index);
+  };
+
   return (
     <>
       <div className='work__filters'>
         {projectsNav.map((item, index) => {
           return (
-            <span className='work__item' key={index}>
+            <span
+              onClick={(event) => {
+                handleClick(event, index);
+              }}
+              className={`${active === index ? 'active-work' : ''} work__item`}
+              key={index}
+            >
               {item.name}
             </span>
           );
         })}
       </div>
       <div className='work__container container grid'>
-        {projectsData.map((item) => {
+        {projects.map((item) => {
           return <WorkItems item={item} key={item.id} />;
         })}
       </div>
