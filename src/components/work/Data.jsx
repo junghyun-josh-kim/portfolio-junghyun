311374eafc708ea864c5ad9051908debe7fe0fc8
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-11-02 21:30:09 -0400

    Portfolio: Data

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,55 @@
+import Project1 from '../../assets/project1.png';
+import Project2 from '../../assets/project2.png';
+import Project3 from '../../assets/project3.png';
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Project1,
+    title: 'Touhenboku Website',
+    category: 'fullstack',
+  },
+  {
+    id: 2,
+    image: Project2,
+    title: 'Mori Website',
+    category: 'frontend',
+  },
+  {
+    id: 3,
+    image: Project3,
+    title: 'Portfolio Website',
+    category: 'frontend',
+  },
+  {
+    id: 4,
+    image: Project1,
+    title: 'Day Manager',
+    category: 'frontend',
+  },
+  {
+    id: 5,
+    image: Project2,
+    title: 'E-commerce Websites',
+    category: 'fullstack, toyproject',
+  },
+  {
+    id: 6,
+    image: Project3,
+    title: 'Twitter Clone',
+    category: 'fullstack, toyproject',
+  },
+  {
+    id: 7,
+    image: Project1,
+    title: 'Youtube Clone',
+    category: 'fullstack, toyproject',
+  },
+];
+
+export const projectsNav = [
+  { name: 'all' },
+  { name: 'frontend' },
+  { name: 'fullstack' },
+  { name: 'toyproject' },
+];
